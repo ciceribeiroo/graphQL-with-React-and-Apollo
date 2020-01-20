bbf5e31bbbbdde8a6abe828e6fa31c4bb6cbbea1
Alice Ribeiro <45270115+ciceribeiroo@users.noreply.github.com>
2020-01-20 12:40:09 -0300

    Add files via upload

--- schema.js
+++ schema.js
@@ -1,47 +1,47 @@
 const axios = require('axios');
 
 const {
-	GraphQLObjectType,
-	GraphQLInt, 
-	GraphQLString,
-	GraphQLBoolean,
-	GraphQLList,
-	GraphQLSchema
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
 } = require('graphql');
 
-//launch tyle
+// Launch Type
 const LaunchType = new GraphQLObjectType({
-	name: 'Launch',
-	fields: ()=>({
-		flight_number:{type: GraphQLInt},
-		mission_name:{type: GraphQLString},
-		launch_year:{type: GraphQLString},
-		launch_date_local:{type: GraphQLString},
-		launch_sucess:{type: GraphQLBoolean},
-		rocket:{type: RocketType}
-	})
+  name: 'Launch',
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType }
+  })
 });
 
-//rocket type
+// Rocket Type
 const RocketType = new GraphQLObjectType({
-	name: 'Rocket',
-	fields: ()=>({
-		rocket_id:{type: GraphQLString},
-		rocket_name:{type: GraphQLString},
-		rocket_type:{type: GraphQLString}
-	})
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
 });
 
-//root query
+// Root Query
 const RootQuery = new GraphQLObjectType({
-	name:'RootQueryType',
-	fields: {
-		launches: {
-		type: new GraphQLList(LaunchType),
-		  resolve(parent, args) {
-			return axios
-			 .get('https://api.spacexdata.com/v3/launches')
-			  .then(res => res.data);
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/launches')
+          .then(res => res.data);
 		  }
 		}
 	  }
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
-const graphqlHTTP= require('express-graphql');
+const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true,
+    formatError: error => ({
+      message: error.message,
+      locations: error.locations,
+      stack: error.stack ? error.stack.split('\n') : [],
+      path: error.path
+  })
   })
 );
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, () => console.log('Server started on port ${PORT }'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
